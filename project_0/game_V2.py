e53bfff18501ac75192e5218ef54239781d713f2
Aleksey55555 <alarabad@gmail.com>
2023-08-08 13:24:02 +0300

    edit description

--- project_0/game_V2.py
+++ project_0/game_V2.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def alg_predict(number:int=1) -> int:
     """The number will be guessed by algorithm
 
@@ -10,26 +11,28 @@ def alg_predict(number:int=1) -> int:
         int: number of attemps to guess
     """
     count = 0
-    predict_number = 50
-    pre_predict_number = 0
+    predict_number = 50 # determine first prediction
+    pre_predict_number = 0 # need for increment and decrement of prediction
     
     while True:
+ 
         count += 1
         temp = predict_number
-        difference = abs(predict_number - pre_predict_number) / 2
-        if difference < 1:
+        difference = abs(predict_number - pre_predict_number) / 2 # increment/decrement of prediction
+        if difference < 1: # round to 1 if increment = 0.5 to pevent loop (prediction = preprediction)
             difference = 1
         
         if number == predict_number:
             break # cycle exit
         elif number > predict_number:
-            predict_number += difference
+            predict_number += difference # increment of prediction
             predict_number = int(predict_number)
             pre_predict_number = temp
         else:
-            predict_number -= difference
+            predict_number -= difference # decrement of prediction
             predict_number = int(predict_number)
             pre_predict_number = temp
+    
     return count
 
 
@@ -48,10 +51,11 @@ def score_game(predict) -> int:
     for number in random_array:
         count_ls.append(predict(number))
     
-    score = np.mean(count_ls)
+    score = np.mean(count_ls) # calculate mean number of attempts
     
     print(f'Average number of attempts to guess the number by this algorithm is {score}')
     return score
+
 
 if __name__ == '__main__':
     # run
